4eaf4bfed9287348490ece6c82d37c4e638d5f0d
WuallanDAvilla <helendavilla2@gmail.com>
2025-08-31 13:51:53 -0300

    Co-authored-by: favo <linxlinx684@gmail.com> Co-authored-by: Ariel Ribeiro <ArielThiago@users.noreply.github.com> Co-authored-by: viniciusbr-star <viniciusbr-star@users.noreply.github.com>

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,3 +49,19 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+function openModal() {
+  const modal = document.getElementById('contactModal');
+  modal.style.display = 'flex';
+  setTimeout(() => {
+      modal.classList.add('show');
+  }, 10);
+}
+
+function closeModal() {
+  const modal = document.getElementById('contactModal');
+  modal.classList.remove('show');
+  setTimeout(() => {
+      modal.style.display = 'none';
+  }, 300);
+}
